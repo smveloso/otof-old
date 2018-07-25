8ba895b39a2561d8efd5d8ad53d497e731e3731b
smv <smveloso@gmail.com>
2018-07-25 16:18:52 -0300

    WIP. BROKEN. Teste para findfotobydigest.

--- src/test/java/org/smveloso/otof/em/PhotoDAOTest.java
+++ src/test/java/org/smveloso/otof/em/PhotoDAOTest.java
@@ -3,6 +3,7 @@ package org.smveloso.otof.em;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.smveloso.otof.model.Photo;
 
 public class PhotoDAOTest {
     
@@ -20,6 +21,15 @@ public class PhotoDAOTest {
         int expResult = 0;
         int result = instance.getTotalPhotoCount();
         assertEquals(expResult, result);
+    }
+
+    @Test
+    public void testFindFotoByDigest()  throws Exception {
+        PhotoDAO instance = PhotoDAO.getInstance();
+        String digest = "0123456789123456789";
+        Photo photo = instance.findFotoByDigest(digest);
+        assertNotNull("Foto nao encontrada por digest.",photo);
+        
     }
     
 }

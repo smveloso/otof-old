33d3235c9f34be55ae4ec686b8f5ef488181d3fb
smv <smveloso@gmail.com>
2018-07-27 18:09:10 -0300

    Eliminando dependencia para o JUNIT.

--- src/test/java/org/smveloso/otof/util/digest/DigestUtilTest.java
+++ src/test/java/org/smveloso/otof/util/digest/DigestUtilTest.java
@@ -1,9 +1,9 @@
 package org.smveloso.otof.util.digest;
 
 import java.io.File;
-import org.junit.Test;
-import static org.junit.Assert.*;
 import org.smveloso.otof.test.TestUtil;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class DigestUtilTest {
     
@@ -15,10 +15,10 @@ public class DigestUtilTest {
     @Test
     public void testGetSha1HexEncoded() throws Exception {
         File arquivo = util.searchFileInClasspath("89db6573a0444b073bf6a454626e1816041e9424");
-        assertTrue("Test file not found.", arquivo.exists());
+        Assert.assertTrue(arquivo.exists(),"Test file not found.");
         String expResult = "89db6573a0444b073bf6a454626e1816041e9424";
         String result = DigestUtil.getSha1HexEncoded(arquivo);
-        assertEquals(expResult.toLowerCase(), result.toLowerCase());
+        Assert.assertEquals(expResult.toLowerCase(), result.toLowerCase());
     }
     
 }

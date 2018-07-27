42c75bf0ee9b88ddacd1a33e9f1bf43599d23a15
smv <smveloso@gmail.com>
2018-07-27 17:20:10 -0300

    Adotando estrategia joined para hierarquia de album.

--- src/main/java/org/smveloso/otof/model/Album.java
+++ src/main/java/org/smveloso/otof/model/Album.java
@@ -7,6 +7,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
@@ -16,6 +20,10 @@ import javax.persistence.Table;
  */
 @Entity
 @Table(name="album")
+@Inheritance(strategy = InheritanceType.JOINED)
+@NamedQueries({
+    @NamedQuery(name="Album.byName", query="from Album a where a.name = :name")
+})
 public abstract class Album implements Serializable {
 
     private static final long serialVersionUID = 1L;

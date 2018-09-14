7e8c090be63ee1956447ae5e1227e42eb8550e9f
smv <smveloso@gmail.com>
2018-09-13 22:02:19 -0300

    shell script para disparar o docker com h2 (producao)

---
+++ docker.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+/usr/bin/docker run -p 9093:1521 -p 8083:81 -v /home/sergio/Env/code/otof-old/h2:/opt/h2-data oscarfonts/h2 java -cp /opt/h2/bin/h2-1.4.197.jar org.h2.tools.Server -trace -web -webAllowOthers -webPort 81  -tcp -tcpAllowOthers -tcpPort 1521  -baseDir /opt/h2-data

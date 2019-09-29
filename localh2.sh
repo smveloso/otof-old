5b0d878499a3d40d0c2e00dc8be9a2bbb9a26d14
sergiomv@home <sergiomv@home>
2019-09-29 15:57:10 -0300

    ifnotexists

--- localh2.sh
+++ localh2.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/bash
-/usr/bin/docker run -d -p 9093:1521 -p 8083:81 -v /home/sergio/Env/code/otof-old/h2:/opt/h2-data oscarfonts/h2 java -cp /opt/h2/bin/h2-1.4.199.jar org.h2.tools.Server -trace -web -webAllowOthers -webPort 81  -tcp -tcpAllowOthers -tcpPort 1521  -baseDir /opt/h2-data
+/usr/bin/docker run --name otof-db -d -p 9093:1521 -p 8083:81 -v /home/sergio/Env/code/otof-old/h2:/opt/h2-data oscarfonts/h2 java -cp /opt/h2/bin/h2-1.4.199.jar org.h2.tools.Server -trace -web -webAllowOthers -webPort 81  -tcp -tcpAllowOthers -tcpPort 1521  -baseDir /opt/h2-data -ifNotExists
